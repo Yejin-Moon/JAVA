fc2404236a508fbce54f15c5e07a1b4c104c3cd6
Yejin-Moon <74597602+Yejin-Moon@users.noreply.github.com>
2024-02-27 15:22:29 +0900

    inputSpace 추가

--- Practice1/Calculator.java
+++ Practice1/Calculator.java
@@ -1,6 +1,22 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+
 public class Calculator extends JFrame{
+
+    private JTextField inputSpace;
     public Calculator() {
+
+        setLayout(null);
+
+        inputSpace = new JTextField();
+        inputSpace.setEditable(false);
+        inputSpace.setBackground(Color.white);
+        inputSpace.setHorizontalAlignment(JTextField.RIGHT);
+        inputSpace.setFont(new Font("Arial",Font.BOLD,50));
+        inputSpace.setBounds(8,10,270,70);
+
+        add(inputSpace);
+        
         setTitle("계산기");
         setVisible(true);
         setSize(300,700);
